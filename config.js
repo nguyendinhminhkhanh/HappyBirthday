d8ff6e97179c226e259e6606d3fb424639757e8d
dinhkhanh <dinhkhanh18@gmail.com>
2023-09-14 03:38:32 +0700

    happy birthday

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Đỗ Quỳnh';
+const giftUrl = '';
+const eventName = ' ᕼᗩᑭᑭY ᗷIᖇTᕼᗞᗩY';
+const titleCard = 'Đỗ Quỳnh';
+const contentCard = 'Chúc mừng bạn bước sang tuổi 20 🎉. Mong rằng những ước mơ, dự định của bạn sẽ trở thành hiện thực trong tuổi mới ! Cười thật nhiều 😃 và mỗi ngày trôi qua, bạn lại càng trưởng thành và đẹp đẽ hơn. Quan trọng là giữ cho bản thân thật nhiều sức khỏe💪. Hãy đón sinh nhật bên người thân và bạn bè thật vui vẻ nhé! 🎈🎈🎈';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = './image/IMG_0615.JPG';
+const giftImage1 = './image/z4691149587985_0cef92efefadb0cbd9ca39e985046f8c.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
